2cf4eee650eb44e7bfdfacf6435378a97e559bb2
www.linkedin.com/in/artur-ulaszek <arthurhurricane5@gmail.com>
2022-08-16 21:58:32 +0200

    Update sd.bat

--- sd.bat
+++ sd.bat
@@ -1,4 +1,9 @@
 @echo off
+echo +++++++++++++++++++++++++++++++++++++++++++++++++++++
+echo Programme shut down the machine at the specific time.
+echo +++++++++++++++++++++++++++++++++++++++++++++++++++++
+@echo off
+echo.
 echo %time%
 :W
 if "%time:~0,2%"=="17" goto :X 
