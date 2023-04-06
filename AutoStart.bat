73275f613f386682b1484913c3f610fa5c135775
www.linkedin.com/in/artur-ulaszek <arthurhurricane5@gmail.com>
2023-04-06 20:22:32 +0200

    Update AutoStart.bat

--- AutoStart.bat
+++ AutoStart.bat
@@ -1,5 +1,8 @@
 @echo off
 
+:: Programme starts selected applications in a planned manner
+:: Creator Artur Ulaszek
+
 cd "C:\Program Files (x86)\Microsoft\Edge\Application\"
 start msedge.exe
 TIMEOUT 2
